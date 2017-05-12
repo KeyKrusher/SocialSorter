4f1d2bd4f27d91214b6958d8f94951fd702cb816
period2 <period2@admins-MacBook-Air-47.local>
2017-05-12 09:35:26 -0500

    Initial Commit

---
+++ SocialSorter/ViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController.swift
+//  SocialSorter
+//
+//  Created by period2 on 5/10/17.
+//  Copyright © 2017 Chubs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var instagramButton: UIButton!
+    @IBOutlet weak var twitterButton: UIButton!
+    @IBOutlet weak var facebookButton: UIButton!
+    
+    
+    @IBAction func instagramButtonTapped(_ sender: UIButton) {
+        
+    }
+    @IBAction func twtterButtonTapped(_ sender: UIButton) {
+        
+    }
+    @IBAction func facebookButtonTapped(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func generateFeed(_ sender: UIButton) {
+        
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Images
+        let instagramLogo = UIImage(named: "instagramLogo")
+        let twitterLogo = UIImage(named: "twitterLogo")
+        let facebookLogo = UIImage(named: "facebookLogo")
+        // Set the images to the buttons
+        instagramButton.setImage(instagramLogo)
+        twitterButton.setImage(twitterLogo)
+        facebookButton.setImage(facebookLogo)
+    }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+    }
+}
+
